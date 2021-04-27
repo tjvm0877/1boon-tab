9ac27a398640c1962bfa2ae195f733b8b1ffd8ee
Hyun <itsmine096@gmail.com>
2021-04-27 14:36:38 +0900

    makeList함수 수정 ( selectedTab으로 현재 활성화 중인 탭을 확인 후 해당 컨텐츠 추가, 더보기 버튼 활성화 여부에 따라 추가할 컨텐츠의 수를 조절), 더보기 버튼 이벤트 수정

--- app.js
+++ app.js
@@ -4,91 +4,68 @@ const $list = document.getElementById('list');
 // 더보기 버튼 showMoreBtn 가져오기
 const $showMoreBtn = document.getElementById('showMoreBtn');
 
-let showmoreCheck = false;
+// 더보기 활성화 여부
+let checkShowMore = false;
 
-// recent.json파일의 정보를 가져오기
-function getPopular() {
-    fetch('recent.json')
+//현재 보고있는 탭
+let selecedTab = document.querySelector('.active');
+
+// 현재 선택한 tab의 API(JSON)정보를 가져오기
+function getJSon() {
+    let jsonName = selecedTab.id;
+    fetch(`${jsonName}.json`)
         .then((response) => response.json())
-        .then((data) => makeList(data, showmoreCheck));
+        .then((data) => makeContents(data, checkShowMore));
 }
 
 // 입력한 json정보를 화면(.list)안에 띄워주기 위한 DOM명령어 처리
-function makeList(data, showmoreCheck) {
-    if (showmoreCheck === false) {
-        for (let i = 0; i < 10; i++) {
-            let url = data[i].url;
-            let img = data[i].img;
-            let title = data[i].title;
-            let cp = data[i].cp;
-            const $li = document.createElement('li');
-
-            const $a = document.createElement('a');
-            $a.href = url;
-
-            const $span = document.createElement('span');
-            $span.classList.add('imageSpan');
-
-            const $img = document.createElement('img');
-            $img.src = img;
-
-            const $titleSpan = document.createElement('span');
-            const $strong = document.createElement('strong');
-            $strong.textContent = title;
-            const $p = document.createElement('p');
-            $p.textContent = cp;
-
-            $span.appendChild($img);
-            $a.appendChild($span);
-
-            $titleSpan.appendChild($strong);
-            $titleSpan.appendChild($p);
-            $a.appendChild($titleSpan);
-
-            $li.appendChild($a);
-            $list.appendChild($li);
-        }
+function makeContents(data, checkShowMore) {
+    let count;
+    if (checkShowMore === false) {
+        count = 0;
     } else {
-        for (let i = 10; i < 20; i++) {
-            let url = data[i].url;
-            let img = data[i].img;
-            let title = data[i].title;
-            let cp = data[i].cp;
-            const $li = document.createElement('li');
+        count = 10;
+    }
+    for (let i = count; i < count + 10; i++) {
+        let url = data[i].url;
+        let img = data[i].img;
+        let title = data[i].title;
+        let cp = data[i].cp;
+        const $li = document.createElement('li');
 
-            const $a = document.createElement('a');
-            $a.href = url;
+        const $a = document.createElement('a');
+        $a.href = url;
 
-            const $span = document.createElement('span');
-            $span.classList.add('imageSpan');
+        const $span = document.createElement('span');
+        $span.classList.add('imageSpan');
 
-            const $img = document.createElement('img');
-            $img.src = img;
+        const $img = document.createElement('img');
+        $img.src = img;
 
-            const $titleSpan = document.createElement('span');
-            const $strong = document.createElement('strong');
-            $strong.textContent = title;
-            const $p = document.createElement('p');
-            $p.textContent = cp;
+        const $titleSpan = document.createElement('span');
+        const $strong = document.createElement('strong');
+        $strong.textContent = title;
+        const $p = document.createElement('p');
+        $p.textContent = cp;
 
-            $span.appendChild($img);
-            $a.appendChild($span);
+        $span.appendChild($img);
+        $a.appendChild($span);
 
-            $titleSpan.appendChild($strong);
-            $titleSpan.appendChild($p);
-            $a.appendChild($titleSpan);
+        $titleSpan.appendChild($strong);
+        $titleSpan.appendChild($p);
+        $a.appendChild($titleSpan);
 
-            $li.appendChild($a);
-            $list.appendChild($li);
-        }
+        $li.appendChild($a);
+        $list.appendChild($li);
     }
 }
 
 // 더보기 버튼 이벤트 등록, 보여주고 더보기 버튼은 숨김처리
 $showMoreBtn.addEventListener('click', function () {
-    showmoreCheck = true;
-    getPopular();
+    checkShowMore = true;
+    getJSon();
     $showMoreBtn.style.display = 'none';
 });
 
-getPopular();
+//첫화면
+getJSon();
